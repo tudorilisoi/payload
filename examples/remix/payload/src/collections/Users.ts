50e97e4331c18fe047a7ef81048ed4518fba02bb
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-07-04 16:05:13 +0300

    translate users
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- examples/remix/payload/src/collections/Users.ts
+++ examples/remix/payload/src/collections/Users.ts
@@ -2,6 +2,17 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  labels: {
+    singular: {
+      en: 'User',
+      ro: 'Utilizator',
+    },
+    plural: {
+      en: 'Users',
+      ro: 'Utilizatori',
+
+    },
+  },
   admin: {
     useAsTitle: 'email',
   },
