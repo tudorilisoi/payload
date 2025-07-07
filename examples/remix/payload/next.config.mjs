c122b5a53d76bcf35cdb82140133f2bc746757bb
Tudor Ilisoi <tudorilisoi@yahoo.com>
2025-07-07 20:17:17 +0300

    add polling config
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- examples/remix/payload/next.config.mjs
+++ examples/remix/payload/next.config.mjs
@@ -3,6 +3,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  webpack: (config, { isServer }) => {
+    config.cache = {
+      type: 'memory',
+      // buildDependencies: {
+      //   config: [__filename], // Cache invalidation on config file change
+      // },
+    };
+    config.watchOptions = {
+      aggregateTimeout: 1000,
+      poll: 1000,
+    }
+    return config;
+  },
 }
 
 export default withPayload(nextConfig)
